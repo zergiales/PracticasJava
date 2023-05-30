7009b652a1d1db8d0a2c74cd5d900b502bbf4532
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2023-05-30 12:54:28 +0200

    subida

--- src/tema1/Variables.java
+++ src/tema1/Variables.java
@@ -1,9 +1,34 @@
 package tema1;
 
 public class Variables {
-
-	public Variables() {
-		// TODO Auto-generated constructor stub
+	// atributos 
+	private int n;
+	private double d;
+	private float f;
+	private Long l;
+	private Byte b;
+	private Short s;
+	
+	/**
+	 *  parametros que hemos utilizado
+	 * @param n
+	 * @param d
+	 * @param f
+	 * @param l
+	 * @param b
+	 * @param s
+	 */
+	public Variables(int n, double d, float f, Long l, Byte b, Short s) {
+		this.n = n;
+		this.d = d;
+		this.f = f;
+		this.l = l;
+		this.b = b;
+		this.s = s;
 	}
+	
+	//constructor 
+	
+	
 
 }
